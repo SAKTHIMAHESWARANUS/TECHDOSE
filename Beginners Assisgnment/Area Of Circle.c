3ce6c205d678d928929ace30a4d06257ff9d2cf5
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2022-09-09 15:18:15 +0530

    Create Area Of Circle.c

---
+++ Beginners Assisgnment/Area Of Circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float radius;
+    scanf("%f",&radius);
+    float area=3.14*(radius*radius);
+    printf("%.2f",area);
+    
+    
+    
+}
