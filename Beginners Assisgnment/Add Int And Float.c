67dc28c643b66688c0ab16e87498c569899a4fba
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2022-09-10 12:19:50 +0530

    Create Add Int And Float.c

---
+++ Beginners Assisgnment/Add Int And Float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    float f;
+    scanf("%d %f",&n,&f);
+    float sum=n+f;
+    printf("%.1f",sum);
+    
+    
+    return 0;
+}
