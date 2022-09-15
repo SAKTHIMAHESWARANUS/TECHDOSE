81d3ae470cdd89c41876016fe6e6c6064e525e5b
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2022-09-15 10:40:28 +0530

    Create  Palindrome Number.java

---
+++ Beginners Assisgnment/Palindrome Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+        return false;
+    }
+    int res=x;
+    int rev=0;
+    while(res>0){
+        rev=(rev*10)+(res%10);
+        res=res/10;
+    }
+    return rev==x;
+
+    }
+}
