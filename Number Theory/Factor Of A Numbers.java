0976783169bc49d9187d5edc92305f8d314b90c9
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2022-09-16 12:37:58 +0530

    Create Factor Of A Numbers.java

---
+++ Number Theory/Factor Of A Numbers.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.print("The factors are: ");
+		for(int i=1;i<n;i++){
+		    if(n%i==0){
+		        System.out.print(i+" ");
+		    }
+		}
+		
+	}
+}
