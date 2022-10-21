95f585c109a5c013df04233eb0344ebded85b195
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2022-10-21 15:13:18 +0530

    Create  Linked List Cycle.c

---
+++ Linked List Assignments/Linked List Cycle.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode * slow=head,*fast=NULL;
+    
+    if(!head || !(head->next))
+        return false;
+    
+    fast=head->next;
+    
+    while(slow!=fast)
+    {
+        if(fast==NULL)
+        {
+            return false;
+        }
+        slow=slow->next;
+        if(fast->next && fast->next->next)
+            fast=fast->next->next;
+        else
+            fast = fast->next;
+    }
+    return true;
+}
