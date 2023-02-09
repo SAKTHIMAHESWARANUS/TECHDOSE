8bb43039aea8aecb48b9a0f99a5ae7fac5a641da
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2023-02-09 15:18:01 +0530

    Update Insertion Sort.java

--- Sorting Algorithms/Insertion Sort.java
+++ Sorting Algorithms/Insertion Sort.java
@@ -1,35 +1,27 @@
-public class InsertionSort {
-	
-	void sort(int arr[])
+import java.util.*;
+class Main
+{
+	public static void main (String[] args)
 	{
-		int n = arr.length;
-		for (int i = 1; i < n; ++i) {
-			int key = arr[i];
-			int j = i - 1;
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
+		
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
 		}
+		for(int i=0;i<arr.length-1;i++){
+		    for(int j=i+1;j>0;j--){
+		        if(arr[j]<arr[j-1]){
+		            int tep=arr[j];
+		            arr[j]=arr[j-1];
+		            arr[j-1]=tep;
+		        }
+		    }
+		}
+		for(int i : arr){
+		    System.out.println(i+" ");
+		}
+		
 	}
-	static void printArray(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-
-		System.out.println();
-	}
-	public static void main(String args[])
-	{
-		int arr[] = { 12, 11, 13, 5, 6 };
-
-		InsertionSort ob = new InsertionSort();
-		ob.sort(arr);
-
-		printArray(arr);
-	}
-};
-
-
+}
