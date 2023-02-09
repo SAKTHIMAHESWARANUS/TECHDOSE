5653b1b6ee398e70033131a4b1ff417e762fbde3
SAKTHIMAHESWARANUS <112954896+SAKTHIMAHESWARANUS@users.noreply.github.com>
2023-02-09 15:51:22 +0530

    Update Cyclic Sort.java

--- Sorting Algorithms/Cyclic Sort.java
+++ Sorting Algorithms/Cyclic Sort.java
@@ -3,27 +3,26 @@ class Main
 {
 	public static void main (String[] args)
 	{
-	    Scanner sc = new Scanner(System.in);
-	    int n=sc.nextInt();
-	    int arr[]=new int[n];
-	    for(int i=0;i<n;i++){
-	        arr[i]=sc.nextInt();
-	    }
-	    int index=0;
-	    while(index<arr.length){
-	        int crt=arr[index]-1;
-	        if(arr[index]!=arr[crt]){
-	            swap(arr,index,crt);
-	        }
-	        else{
-	            index++;
-	        }
-	    }
-	    for(int i=0;i<n;i++) System.out.print(arr[i]+" ");
-	}
-	public static void swap(int arr[],int ind,int crt){
-	    int temp=arr[ind];
-	    arr[ind]=arr[crt];
-	    arr[crt]=temp;
+		
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int index=0;
+		for(;index<arr.length;){
+		    int crt=arr[index]-1;
+		    if(arr[crt]!=arr[index]){
+		        int temp=arr[crt];
+		        arr[crt]=arr[index];
+		        arr[index]=temp;
+		    }
+		    else{
+		        index++;
+		    }
+		}
+		System.out.print(Arrays.toString(arr));
+		
 	}
 }
